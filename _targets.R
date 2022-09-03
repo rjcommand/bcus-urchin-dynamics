43d28d032cf78cb50a4f3b1224dec89024e8c16c
Rylan Command <rylancommand95@gmail.com>
2022-09-03 11:35:02 -0230

    Add aic table, figure 2, and urchin model summary table

--- _targets.R
+++ _targets.R
@@ -11,9 +11,10 @@ tar_plan(
   tar_target(bcus_modeling_data, clean_bcus_data(bcus_data)),
   tar_target(urchin_statistics_table, summary_statistics_table(bcus_modeling_data)),
   tar_target(urchin_models, fit_urchin_models(bcus_modeling_data)),
-  tar_target(aic_table, get_aic_table(urchin_models))
-# target = function_to_make(arg), ## drake style
-
-# tar_target(target2, function_to_make2(arg)) ## targets style
-
+  tar_target(aic_table, get_aic_table(urchin_models)),
+  tar_target(fig2, urchin_model_timeseries_plot(bcus_modeling_data, urchin_models$env_m1I)),
+  tar_target(urchin_model_summary_table, model_summary_table(urchin_models$env_m1I)),
+    
+  tar_render(analysis, "doc/analysis.Rmd")
+  
 )
