a45df4b039bf402e7518e66e0a75961fc94aba7a
Rylan Command <rylancommand95@gmail.com>
2022-12-16 11:42:24 -0330

    Add packages

--- packages.R
+++ packages.R
@@ -4,9 +4,9 @@ library(dotenv)
 library(targets)
 library(tarchetypes)
 library(tidyverse)
-library(lubridate)
-library(mgcv)
-library(gratia)
+library(lubridate)  # To work with date-time formats
+library(mgcv)  # Fit and evaluate GAMs
+library(gratia)  # Tidy GAM visualization
 library(data.table)
 library(rmarkdown)
 library(kableExtra)
@@ -14,3 +14,11 @@ library(tidymv)
 library(mgcViz)
 library(ncdf4)  # Reading netCDF files (for chlorophyll-a data)
 library(raster)  # Working with raster data (for chlorophyll-a data and bathymetry)
+
+library(PMCMRplus)  # For non-parametric multiple comparisons
+library(ggspatial)  # For scale bar and north arrow on maps
+library(mapdata)    # For North America base map
+library(maps)       # For plotting maps
+
+library(terra)  # For rasters
+library(basemaps)  # For plotting basemaps (Figure S1)
